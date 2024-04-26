faac4eddef3e666f796b9b972fe6e13e1366e487
liu1073811240 <1073811240@qq.com>
2024-04-26 17:15:41 +0800

    定义模型类

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'hui0202',  # 数据库用户密码
+        'NAME': 'book'  # 数据库名字
     }
 }
 
